48315c611a9be0d523816cfd6ee914458b74138f
Shantanu2k19 <shantanusingh2030@gmail.com>
2024-03-20 00:16:42 +0530

    basic

---
+++ mymedic/app/home/page.tsx
@@ -0,0 +1,7 @@
+export default function hello(){
+    return (
+        <>
+        hello
+        </>
+    )
+}
--- mymedic/app/page.tsx
+++ mymedic/app/page.tsx
@@ -1,9 +1,17 @@
 //import Image from "next/image";
+import Link from "next/link";
+import './globals.css'
 
 export default function Home() {
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     MyMedic
+      <Link className="text-sm mt-3 text-right" href={'/home'}>
+        MyMedic
+        <span className="home">
+            Login
+        </span>
+      </Link>
     </main>
   );
 }
