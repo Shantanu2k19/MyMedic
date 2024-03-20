e354e7d3d3e96ab466c18b59bf5d5482fb1b9e12
Shantanu2k19 <shantanusingh2030@gmail.com>
2024-03-20 22:04:39 +0530

    initial commit

--- mymedic/app/(pdp)/page.tsx
+++ mymedic/app/(pdp)/page.tsx
@@ -1,15 +1,15 @@
 //import Image from "next/image";
 import Link from "next/link";
-import './globals.css'
+import '../globals.css'
 
-export default function Home() {
-
+export default function Root() {
+  console.log("root-page.tsx")
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Link className="text-sm mt-3 text-right" href={'/home'}>
-        MyMedic
-        <span className="home">
-            Login
+        MyMedic 
+        <span className="underline">
+            [ Login ]
         </span>
       </Link>
     </main>
